82209e3bfab54a49d4184c942e0f2e7414d85dbc
Алсу Фаизова <silicium.xxx@gmail.com>
2022-07-03 07:41:53 +0300

    added

---
+++ app/src/main/java/com/example/summerpractice22/MainFragment.kt
@@ -0,0 +1,16 @@
+package com.example.summerpractice22
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.summerpractice22.R
+import com.example.summerpractice22.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+
+}
